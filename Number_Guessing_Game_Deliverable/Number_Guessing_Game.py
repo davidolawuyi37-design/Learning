2145174519d9fc2e4d792088df116c0ffec1b3fa
davidolawuyi37 <davidolawuyi37@gmail.com>
2025-12-26 02:04:33 +0100

    Updated

--- Number_Guessing_Game_Deliverable/Number_Guessing_Game.py
+++ Number_Guessing_Game_Deliverable/Number_Guessing_Game.py
@@ -6,10 +6,10 @@ Description: A simple game where the user guesses a randomly generated number.
 
 import random
 # Generate a random number between 1 and 10
-Secret_Number = random.randint(1, 10)
+Secret_Number = random.randint(1, 200)
 
 print("Welcome To The Number Guessing Game!")
-print("I have chosen a number between 1 and 10.")
+print("I have chosen a number between 1 and 200.")
 print("Try to guess it!\n")
 
 #Loop until the correct number is guessed
@@ -22,4 +22,4 @@ while True:
         print("Too low! Try again.\n")
     else:
         print("Congratulations! You guessed the correct number!")
-        break
+    break
